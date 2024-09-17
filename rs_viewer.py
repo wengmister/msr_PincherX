e36629bb657cbf4b40766b787426d1b42c69ab41
wengmister <wengmister@gmail.com>
2024-09-17 15:15:03 -0500

    updated viewer streamer

--- rs_viewer.py
+++ rs_viewer.py
@@ -1,6 +1,7 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 class Viewer:
     def __init__(self, enable_record = False, enable_playback = False, filename = None):
@@ -36,14 +37,29 @@ class Viewer:
         # set clipping distance in meters
         self.set_clipping_distance(1)
 
-
-
     def get_meter_distance(self, camera_unit):
         return camera_unit * self.depth_scale
 
     def set_clipping_distance(self, threshold):
         clipping_distance_in_meters = threshold # in meters
         self.clipping_distance = clipping_distance_in_meters / self.depth_scale
+
+    def get_frames(self):
+        # Get frameset of color and depth
+        frames = self.pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
+
+        # Align the depth frame to color frame
+        aligned_frames = self.align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        return depth_image, color_image
+
 
     def stream(self):
         try:
@@ -90,10 +106,21 @@ class Viewer:
 if __name__=="__main__":
 
     # test_viewer = Viewer(enable_playback=True, filename="test_record.bag")
+    
     test_viewer = Viewer()
-    test_viewer.stream()
+
+    depth, color = test_viewer.get_frames()
+    print(depth.shape)
+    print(color.shape)
+
+    color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV) #
+    # plt.imshow(color)
+    # plt.show()
+    
+    # test_viewer.stream()
 
 
 
 
 ## [1]"librealsense/wrappers/python/examples/align-depth2color.py at master · IntelRealSense/librealsense · GitHub", Github, Available: https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/align-depth2color.py. [Accessed 17 September. 2024].
+## [2]"OpenCV: Changing Colorspaces", Docs, Available: https://docs.opencv.org/4.6.0/df/d9d/tutorial_py_colorspaces.html. [Accessed 17 September. 2024].
