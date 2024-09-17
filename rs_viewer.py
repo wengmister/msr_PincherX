6af2f2d70eef6f8cc272f0ee7ed903bd31139ecc
wengmister <wengmister@gmail.com>
2024-09-17 16:04:50 -0500

    functional masked stream

--- rs_viewer.py
+++ rs_viewer.py
@@ -2,6 +2,8 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from mask import *
+
 
 class Viewer:
     def __init__(self, enable_record = False, enable_playback = False, filename = None):
@@ -103,20 +105,69 @@ class Viewer:
         finally:
             self.pipeline.stop()
 
+    def masked_stream(self, color_str):
+        try:
+            while True:
+                # Get frameset of color and depth
+                frames = self.pipeline.wait_for_frames()
+                # frames.get_depth_frame() is a 640x360 depth image
+
+                # Align the depth frame to color frame
+                aligned_frames = self.align.process(frames)
+
+                # Get aligned frames
+                aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+                color_frame = aligned_frames.get_color_frame()
+
+                # Validate that both frames are valid
+                if not aligned_depth_frame or not color_frame:
+                    continue
+
+                depth_image = np.asanyarray(aligned_depth_frame.get_data())
+                color_image = np.asanyarray(color_frame.get_data())
+                
+                mask, _ = mask_hsv(color_image, color_dict_HSV[color_str][1], color_dict_HSV[color_str][0])
+                print(mask.shape)
+
+
+                # Remove background - Set pixels further than clipping_distance to grey
+                # grey_color = 153
+                # depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels -> expanding dimensionality to enable comparison
+                # bg_removed = np.where((depth_image_3d > self.clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+                # Render images:
+                #   depth align to color on left
+                #   depth on right
+                depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+                # images = np.hstack((mask, depth_colormap))
+
+                # cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
+                cv2.imshow('Align Example', mask)
+                key = cv2.waitKey(1)
+                # Press esc or 'q' to close the image window
+                if key & 0xFF == ord('q') or key == 27:
+                    cv2.destroyAllWindows()
+                    break
+        finally:
+            self.pipeline.stop()
+
+
+
 if __name__=="__main__":
 
     # test_viewer = Viewer(enable_playback=True, filename="test_record.bag")
     
     test_viewer = Viewer()
+    test_viewer.masked_stream("purple")
 
     depth, color = test_viewer.get_frames()
 
     print(depth.shape)
     print(color)
 
-    color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV) #
-    plt.imshow(color)
-    plt.show()
+    # color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV) #
+    # plt.imshow(color)
+    # plt.show()
     
     # test_viewer.stream()
 
